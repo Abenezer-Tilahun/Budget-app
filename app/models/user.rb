6c0c7975288d58de4c6b66e9573b8c86f84c55ec
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-07-06 16:57:59 +0300

    Create home page and page to add category page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :groups, foreign_key: 'user_id'
   has_many :entities, foreign_key: 'entity_id'
+  has_one_attached :avatar, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
