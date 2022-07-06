27571d387a738b28fdb48f74f9983acd68069318
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-07-06 16:58:42 +0300

    Adjust route for home page and category page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'splash/index'
+
   # Defines the root path route ("/")
-  root "splash#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+    unauthenticated { root 'splash#index', as: :unauthenticated_root }
+  end
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   resources :splash, only:[:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :income, :update, :destroy]
+  resources :groups, only: [:index,:show , :new, :create, :destroy]
 end
